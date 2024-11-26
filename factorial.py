ba47fc613c2a5d4ec633b655f2d724f57b3cd79d
Mohini692 <mohinipatil2423@gmail.com>
2024-11-26 22:47:31 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,9 @@
+import math
+
+# Input from the user
+num = int(input("Enter a number: "))
+
+# Calculate factorial using math.factorial
+result = math.factorial(num)
+
+print(f"The factorial of {num} is {result}")
